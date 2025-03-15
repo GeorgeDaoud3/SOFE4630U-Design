e644409808a6b1749ec30b9b6e68b1e24da5cab3
George.Daoud@OntarioTechu.net <GodIsLove@3>
2025-03-15 15:11:19 -0400

    adjust input/output names

--- docker/depth_cam1/main.py
+++ docker/depth_cam1/main.py
@@ -3,37 +3,50 @@ import depth_pro
 import numpy as np
 import torch
 
+# inputs
+
+Occluded_Image_View, _, _ = depth_pro.load_rgb("./A_001.png")
+Pedestrians=np.array([[1010,654,1132,1080]],dtype=np.int32);
+
+################################################################################################################
+
 f_px=2200
-boxes=np.array([[1010,654,1132,1080]],dtype=np.int32);
+
 threshold=10;
 # Load model and preprocessing transform
 model, transform = depth_pro.create_model_and_transforms()
 model.eval()
 
 # Load and preprocess an image.
-image, _, _ = depth_pro.load_rgb("./A_001.png")
-image = transform(image)
+
+Occluded_Image_View = transform(Occluded_Image_View)
 
 # Run inference.
-prediction = model.infer(image, f_px=torch.Tensor([f_px]))
+prediction = model.infer(Occluded_Image_View, f_px=torch.Tensor([f_px]))
 depth = prediction["depth"]  # Depth in [m].
 focallength_px = prediction["focallength_px"]  # Focal length in pixels.
-print("focallength_px : "+str(focallength_px))
 # Convert depth to numpy array
 depth_np = depth.squeeze().cpu().numpy()
 close_objects=[]
 depths=[]
-for i in range(boxes.shape[0]):
+for i in range(Pedestrians.shape[0]):
 	#print("box :" + str(i))
-	x1, y1, x2, y2=boxes[i,:];
+	x1, y1, x2, y2=Pedestrians[i,:];
 	# Extract depth value at the center of the bounding box
 	depth_value = np.median(depth_np[y1:y2, x1:x2])
 	#print("Depth:" + str(depth_value))
 	if(depth_value<threshold):
-		close_objects.append(boxes[i,:])
+		close_objects.append(Pedestrians[i,:])
 		depths.append(depth_value)
+
+depths=np.array(depths)
+Pedestrians=np.array(close_objects).astype(np.int32);
+
+################################################################################################################
+
+# outputs
 print("Close objects")
-print(close_objects)
+print(Pedestrians)
 print("their depth")
 print(depths)
 
--- docker/depth_cam2/main.py
+++ docker/depth_cam2/main.py
@@ -3,40 +3,52 @@ import depth_pro
 import numpy as np
 import torch
 
-f_px=2500
-boxes=np.array([[1311, 541, 1919, 931],
+# inputs
+
+Occluding_Image_View, _, _ = depth_pro.load_rgb("./B_001.png")
+vehicles=np.array([[1311, 541, 1919, 931],
 	 	[256, 540, 373, 613],
 	 	[374, 542, 474, 606],
 	 	[469, 544, 549, 594]],dtype=np.int32);
+
+################################################################################################################
+
+f_px=2500
+
 threshold=20;
 # Load model and preprocessing transform
 model, transform = depth_pro.create_model_and_transforms()
 model.eval()
 
 # Load and preprocess an image.
-image, _, _ = depth_pro.load_rgb("./B_001.png")
-image = transform(image)
+
+Occluding_Image_View = transform(Occluding_Image_View)
 
 # Run inference.
-prediction = model.infer(image, f_px=torch.Tensor([f_px]))
+prediction = model.infer(Occluding_Image_View, f_px=torch.Tensor([f_px]))
 depth = prediction["depth"]  # Depth in [m].
 focallength_px = prediction["focallength_px"]  # Focal length in pixels.
-print("focallength_px : "+str(focallength_px))
 # Convert depth to numpy array
 depth_np = depth.squeeze().cpu().numpy()
 close_objects=[]
 depths=[]
-for i in range(boxes.shape[0]):
+for i in range(vehicles.shape[0]):
 	#print("box :" + str(i))
-	x1, y1, x2, y2=boxes[i,:];
+	x1, y1, x2, y2=vehicles[i,:];
 	# Extract depth value at the center of the bounding box
 	depth_value = np.median(depth_np[y1:y2, x1:x2])
 	#print("Depth:" + str(depth_value))
 	if(depth_value<threshold):
-		close_objects.append(boxes[i,:])
+		close_objects.append(vehicles[i,:])
 		depths.append(depth_value)
+
+depths=np.array(depths)
+vehicles=np.array(close_objects).astype(np.int32);
+
+################################################################################################################
+
+# outputs
 print("Close objects")
-print(close_objects)
+print(vehicles)
 print("their depth")
 print(depths)
-
