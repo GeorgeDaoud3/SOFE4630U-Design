0c926b6d7b0252138aa53720a0f9dc951042632f
George.Daoud@OntarioTechu.net <GodIsLove@3>
2025-03-15 14:48:40 -0400

    add comments

--- docker/Yolo_pedestrian/main.py
+++ docker/Yolo_pedestrian/main.py
@@ -1,18 +1,21 @@
 from ultralytics import YOLO
 import cv2
 
+# input
+Occluded_Image_View = cv2.imread("./A_001.png")
+
+################################################################################################################
+
 # Load a model
 model = YOLO("./yolo11n.pt")  # load an official detection model
 
 # predict and locate object
-im2 = cv2.imread("./A_001.png")
-results = model.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
+results = model.predict(source=Occluded_Image_View, save=True, save_txt=True)  # save predictions as labels
 
 # print results
 # Process results list
 person_boxes = []
 for result in results:
-    print(result)
     boxes = result.boxes.xyxy.cpu().numpy() # Get bounding boxes
     classes = result.boxes.cls.cpu().numpy() # Get class labels
     confs = result.boxes.conf.cpu().numpy() # Get class labels
@@ -23,5 +26,9 @@ for box, cls, conf in zip(boxes, classes,confs):
 	    count+=1;
 	    print("\t box : "+str(box[:4]))
 	    x1, y1, x2, y2 = map(int, box[:4])
-	    print("\t cls : "+str(cls))
-	    print("\t conf : "+str(conf))
+	    person_boxes.append(box[:4])
+        
+################################################################################################################        
+
+#ouput
+print("output : "+str(person_boxes));
